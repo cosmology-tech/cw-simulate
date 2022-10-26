8f896ab14e66f6f4330476a3af0589290f73fe25
William Chen <william@terran.one>
2022-10-26 04:54:03 -0700

    add reply test

--- src/contract-tests.spec.ts
+++ src/contract-tests.spec.ts
@@ -3,6 +3,95 @@ import { CWSimulateApp } from './CWSimulateApp';
 
 const testBytecode = readFileSync('testing/cw_simulate_tests-aarch64.wasm');
 
+interface MsgCommand {
+  msg: any;
+}
+
+enum ReplyOn {
+  SUCCESS = 'success',
+  AlWAYS = 'always',
+  ERROR = 'error',
+  NEVER = 'never',
+}
+
+interface SubCommand {
+  sub: [number, any, ReplyOn];
+}
+
+interface EvCommand {
+  ev: [string, [string, string][]];
+}
+
+interface AttrCommand {
+  attr: [string, string];
+}
+
+interface DataCommand {
+  data: number[];
+}
+
+interface ThrowCommand {
+  throw: string;
+}
+
+type Command =
+  | MsgCommand
+  | SubCommand
+  | EvCommand
+  | AttrCommand
+  | DataCommand
+  | ThrowCommand;
+
+function run(program: Command[]) {
+  return {
+    run: {
+      program,
+    },
+  };
+}
+
+function msg(payload: any): MsgCommand {
+  return {
+    msg: payload,
+  };
+}
+
+function sub(id: number, msg: any, reply_on: ReplyOn): SubCommand {
+  return {
+    sub: [id, msg, reply_on],
+  };
+}
+
+function ev(ty: string, attrs: [string, string][]): EvCommand {
+  return {
+    ev: [ty, attrs],
+  };
+}
+
+function attr(k: string, v: string): AttrCommand {
+  return {
+    attr: [k, v],
+  };
+}
+
+function data(v: number[]): DataCommand {
+  return {
+    data: v,
+  };
+}
+
+function push(data: string) {
+  return {
+    push: { data },
+  };
+}
+
+function err(msg: string): ThrowCommand {
+  return {
+    throw: msg,
+  };
+}
+
 describe('CWSimulate Contract Tests', function () {
   it('works', async () => {
     let app = new CWSimulateApp({
@@ -20,19 +109,11 @@ describe('CWSimulate Contract Tests', function () {
     let { contractAddress } = res;
     console.log(res);
 
-    let executeMsg = {
-      run: {
-        program: [
-          {
-            msg: {
-              push: {
-                data: 'Hello',
-              },
-            },
-          },
-        ],
-      },
-    };
+    let executeMsg = run([
+      sub(1, run([msg(push('S1'))]), ReplyOn.SUCCESS),
+      sub(2, run([err('s2')]), ReplyOn.ERROR),
+      msg(push('M1')),
+    ]);
 
     res = await app.wasm.execute(
       info.sender,
